903ed4709b2a2d7896262c4105a09c308c002db4
Fly.李飞翔 <1985228464@qq.com>
2016-06-09 22:52:47 +0800

    添加轮播图类FlyCarouselView

--- FlyKitHeader.h
+++ FlyKitHeader.h
@@ -19,19 +19,39 @@
  
  */
 
+
 #import "FlyRSA.h"                          //RSA加解密
+
 #import "FlyMD5.h"                          //MD5加密
+
 #import "NSObject+FlyModel.h"               //json转model
+
 #import "FlyRegex.h"                        //正则
+
 #import "FlyPhoto.h"                        //点击图片展示大图
-#import "FlyScrollview.h"                   //输入框自适应
+
+#import "FlyKeyboard.h"                     //输入框自适应
+
 #import "UIView+Frame.h"                    //方便的改frame
+
 #import "NSArray+FlyOperation.h"            //数组方法
+
 #import "UITextField+FlyOperation.h"        //输入框方法
+
 #import "FlyViewController.h"               //vc基类
+
 #import "FlyNavigationController.h"         //nvc基类
+
 #import "FlyWhetherHaveNet.h"               //判断是否有网络
+
 #import "FlyGetIPAddress.h"                 //获取IP
 
+#import "FlyCarouselView.h"                 //轮播图
+
+
+
+
 #endif
+
 #endif /* FlyKitHeader_h */
+
